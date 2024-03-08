7067d8f776513ee154911fe07060c5372320a26f
ArtemZayarny <artyom.zayarny@gmail.com>
2024-03-08 21:12:28 +0200

    enable cors

--- apps/todo/src/main.ts
+++ apps/todo/src/main.ts
@@ -3,6 +3,7 @@ import { TodoModule } from './todo.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(TodoModule);
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
